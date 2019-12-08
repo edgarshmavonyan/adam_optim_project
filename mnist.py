66393520acb6de72079f2ea6a2ec9e9e5864a683
Savelii Kozhevnikov <let4ik@let4ik-MacBook.local>
2019-12-08 22:40:09 +0300

    MNIST

--- mnist.py
+++ mnist.py
@@ -43,9 +43,9 @@ def train(model, device, train_loader, optimizer, epoch, log_interval=10):
         loss.backward()
         optimizer.step()
         if batch_idx % log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
+            # print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            #    epoch, batch_idx * len(data), len(train_loader.dataset),
+            #    100. * batch_idx / len(train_loader), loss.item()))
             loss_list.append(loss.item())
 
     return loss_list
