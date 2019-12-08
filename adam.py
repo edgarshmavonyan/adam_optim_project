6ee2a043ebd6edbc9cf450c767179455e5eee49d
edgarshmavonyan <shmavonyan.eha@phystech.edu>
2019-12-08 19:17:47 +0300

    WIP: simple adam

--- adam.py
+++ adam.py
@@ -4,8 +4,14 @@ import torch
 class AdamOptimizer(torch.optim.Optimizer):
     def __init__(self, params, lr=1e-3, beta=(0.9, 0.999),
                  eps=1e-8, l2=0):
-        defaults = dict(lr=lr, beta=beta, eps=eps, l2=l2)
-        super().__init__(params, defaults)
+        self.lr = lr
+        self.beta1, self.beta2 = beta
+        self.eps = eps
+        self.l2 = l2
+        self.params = params
+        self.__avg = tuple([None] * len(params))
+        self.__sq_avg = tuple([None] * len(params))
+        self.__step = 0
 
     def step(self, closure=None):
 
@@ -13,39 +19,28 @@ class AdamOptimizer(torch.optim.Optimizer):
         if closure is not None:
             loss = closure()
 
-        for param_group in self.param_groups:
-            for param in param_group['params']:
-                if param.grad is None:
-                    continue
-                grad = param.grad.data
-                state = self.state[param]
+        for i, param in enumerate(self.params):
+            avg, sq_avg = self.__avg[i], self.__sq_avg[i]
+            grad = param.grad
 
-                if len(state) == 0:
-                    # init state
-                    state['step'] = 0
-                    state['avg'] = torch.zeros_like(param.data)
-                    state['sq_avg'] = torch.zeros_like(param.data)
+            if self.__step == 0:
+                avg = torch.zeros_like(param)
+                sq_avg = torch.zeros_like(param)
 
-                # load current state
-                state['step'] += 1
-                step = state['step']
-                avg = state['avg']
-                sq_avg = state['sq_avg']
-                beta1, beta2 = param_group['beta']
-                eps = param_group['eps']
-                lr = param_group['lr']
-                l2 = param_group['l2']
+            # perform step
+            self.__step += 1
+            if self.l2 != 0:
+                with torch.no_grad():
+                    grad.add_(self.l2 * param)
 
-                # perform step
-                if l2 != 0:
-                    grad.add_(l2 * param.data)
-                avg.mul_(beta1)
-                avg.add_((1 - beta1) * grad)
-                sq_avg.mul_(beta2)
-                sq_avg.add_((1 - beta2) * grad**2)
-                unbiased_avg = avg / (1 - beta1**step)
-                unbiased_sq_avg = sq_avg / (1 - beta2**step)
-                quotient = unbiased_avg / (unbiased_sq_avg.sqrt() + eps)
-                param.data.sub_(lr * quotient)
+            avg.mul_(self.beta1)
+            avg.add_((1 - self.beta1) * grad)
+            sq_avg.mul_(self.beta2)
+            sq_avg.add_((1 - self.beta2) * grad ** 2)
+            unbiased_avg = avg / (1 - self.beta1 ** self.__step)
+            unbiased_sq_avg = sq_avg / (1 - self.beta2 ** self.__step)
+            quotient = unbiased_avg / (unbiased_sq_avg.sqrt() + self.eps)
+            with torch.no_grad():
+                param.sub_(self.lr * quotient)
 
         return loss
