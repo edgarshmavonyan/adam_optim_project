f4800d8bee28f13dd531e034a1dd54ccac4e8793
Savelii Kozhevnikov <let4ik@let4ik-MacBook.local>
2019-12-08 21:59:29 +0300

    MNIST

---
+++ mnist.py
@@ -0,0 +1,83 @@
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+#from adam import AdamOptimizer
+from torch.optim.lr_scheduler import StepLR
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout2d(0.25)
+        self.dropout2 = nn.Dropout2d(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+
+def train(model, device, train_loader, optimizer, epoch, log_interval=10):
+    loss_list = []
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+            loss_list.append(loss.item())
+
+    return loss_list
+
+
+def MnistTest(optimizer, **optimizer_params):
+    # Training settings
+    batch_size = 64
+    epochs = 1
+    use_cuda = torch.cuda.is_available()
+
+    torch.manual_seed(42)
+
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=batch_size, shuffle=True, **kwargs)
+
+    model = Net().to(device)
+    optimizer = optimizer(model.parameters(), **optimizer_params)
+
+    loss = []
+    #scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+    for epoch in range(1, epochs + 1):
+        cur_loss = train(model, device, train_loader, optimizer, epoch)
+        loss.extend(cur_loss)
+        #scheduler.step()
+
+#MnistTest(AdamOptimizer, )
