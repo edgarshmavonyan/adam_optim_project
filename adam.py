997972e6857f6c53639950a692813728efce63d6
edgarshmavonyan <shmavonyan.eha@phystech.edu>
2019-12-08 19:22:15 +0300

    fix simple adam

--- adam.py
+++ adam.py
@@ -9,9 +9,9 @@ class AdamOptimizer(torch.optim.Optimizer):
         self.eps = eps
         self.l2 = l2
         self.params = params
-        self.__avg = tuple([None] * len(params))
-        self.__sq_avg = tuple([None] * len(params))
-        self.__step = 0
+        self.__avg = [None] * len(params)
+        self.__sq_avg = [None] * len(params)
+        self.__step = [0] * len(params)
 
     def step(self, closure=None):
 
@@ -20,15 +20,17 @@ class AdamOptimizer(torch.optim.Optimizer):
             loss = closure()
 
         for i, param in enumerate(self.params):
-            avg, sq_avg = self.__avg[i], self.__sq_avg[i]
             grad = param.grad
 
-            if self.__step == 0:
-                avg = torch.zeros_like(param)
-                sq_avg = torch.zeros_like(param)
+            if self.__step[i] == 0:
+                self.__avg[i] = torch.zeros_like(param)
+                self.__sq_avg[i] = torch.zeros_like(param)
+
+            avg, sq_avg = self.__avg[i], self.__sq_avg[i]
 
             # perform step
-            self.__step += 1
+            self.__step[i] += 1
+            step = self.__step[i]
             if self.l2 != 0:
                 with torch.no_grad():
                     grad.add_(self.l2 * param)
@@ -37,8 +39,8 @@ class AdamOptimizer(torch.optim.Optimizer):
             avg.add_((1 - self.beta1) * grad)
             sq_avg.mul_(self.beta2)
             sq_avg.add_((1 - self.beta2) * grad ** 2)
-            unbiased_avg = avg / (1 - self.beta1 ** self.__step)
-            unbiased_sq_avg = sq_avg / (1 - self.beta2 ** self.__step)
+            unbiased_avg = avg / (1 - self.beta1 ** step)
+            unbiased_sq_avg = sq_avg / (1 - self.beta2 ** step)
             quotient = unbiased_avg / (unbiased_sq_avg.sqrt() + self.eps)
             with torch.no_grad():
                 param.sub_(self.lr * quotient)
